45952d9aeec05c7a575004e596ab72574330335c
khacbac <hokhacbac@gmail.com>
2024-01-04 20:45:48 +0700

    added react-navigation

---
+++ src/components/AppContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export const AppContainer = ({ children }) => {
+  return <View style={styles.container}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
--- src/features/onboarding/OnBoardingScreen.js
+++ src/features/onboarding/OnBoardingScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { AppContainer } from "../../components";
 
 export const OnBoardingScreen = () => {
   return (
-    <View>
+    <AppContainer>
       <Text>OnBoardingScreen</Text>
-    </View>
+    </AppContainer>
   );
 };
