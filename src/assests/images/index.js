d3019b7df5505f95f8de8d0cea679ff9d7bc3be2
khacbac <hokhacbac@gmail.com>
2024-01-04 20:27:11 +0700

    added images

---
+++ src/assests/images/index.js
@@ -0,0 +1,11 @@
+export const AppImages = {
+  bruschetta: require("./bruschetta.png"),
+  "delivery-van": require("./delivery-van.png"),
+  "greek-salad": require("./greek-salad.png"),
+  "grilled-fish": require("./grilled-fish.png"),
+  "hero-image": require("./hero-image.png"),
+  "lemon-dessert": require("./lemon-dessert.png"),
+  logo: require("./logo.png"),
+  pasta: require("./pasta.png"),
+  profile: require("./profile.png"),
+};
