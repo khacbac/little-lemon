6f6b38215fd7c4bd7735164d2a12eab7d3aefe46
khacbac <hokhacbac@gmail.com>
2024-01-04 20:40:02 +0700

    added app colors

---
+++ src/assests/colors/index.js
@@ -0,0 +1,8 @@
+export const AppColors = {
+  primary1: "#495E57",
+  primary2: "#F4CE14",
+  secondary1: "#EE9972",
+  secondary2: "#495E57",
+  secondary3: "#FBDABB",
+  secondary4: "#333333",
+};
